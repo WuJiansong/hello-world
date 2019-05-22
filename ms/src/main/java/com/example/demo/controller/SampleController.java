80b21623506fdf8a89a4568a2ea3e962ce1dba43
wjs <wjs@DruidZMacBook-Pro.local>
2019-05-22 17:49:57 +0800

    no message

--- ms/src/main/java/com/example/demo/controller/SampleController.java
+++ ms/src/main/java/com/example/demo/controller/SampleController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
 public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "helloworld!";
+    }
+
+    public static void main(String[] args)throws Exception{
+        SpringApplication.run(SampleController.class,args);
+    }
 }
